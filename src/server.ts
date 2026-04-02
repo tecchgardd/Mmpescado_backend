84a4a76f2f4d1e0691cbc2118f2951ca44e137d8
techchgard <techgard.comercial@gmail.com>
2026-04-02 08:32:31 -0300

    alteração vercel.json

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import app from "./app.js";
 
-const PORT = 3333;
+if (process.env.NODE_ENV !== "production") {
+  const PORT = 3333;
+  app.listen(PORT, () => {
+    console.log(`🔥 Server rodando em http://localhost:${PORT}`);
+    console.log(`📚 Swagger em http://localhost:${PORT}/docs`);
+  });
+}
 
-app.listen(PORT, () => {
-  console.log(`🔥 Server rodando em http://localhost:${PORT}`);
-  console.log(`📚 Swagger em http://localhost:${PORT}/docs`);
-});
+export default app; // ← Vercel usa isso em produção
