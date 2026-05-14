b34e22177d8f052b2a53ffd8e497e923bfb99914
tecchgardd <techgard.comercial@gmail.com>
2026-05-14 15:10:06 -0300

    Update auth.ts

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -11,6 +11,7 @@ export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL!,
 
   trustedOrigins: [
+    "http://127.0.0.1:5173",
     'http://localhost:5173',
     'https://mm-pescados-front.vercel.app',
   ],
